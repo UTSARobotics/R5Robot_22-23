caf1a59794a36656ecb1dc8ebcebad6866485762
JC <109114226+jc-cr@users.noreply.github.com>
2023-01-27 15:46:42 -0600

    Delete src directory

--- src/misc/AutonomousAirVehicleFolder/AerialDesignAutonomy/test.py
+++
@@ -1,3 +0,0 @@
-print("hello world")
-print("test #3") #10/18/2022
-#test4
--- src/script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Ros Test START"
-source /opt/ros/noetic/bash/setup.bash
-source /home/user/catkin_ws/devel/setup.bash
-roscd
-pwd
-echo "Ros test END"
